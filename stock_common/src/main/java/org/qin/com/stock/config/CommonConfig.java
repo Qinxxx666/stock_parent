0351c84f425e28c8bf290c06c317796787b3f806
Qin <qiqin@hbbohua.com>
2025-02-12 17:13:04 +0800

    添加amqp和CaffeineCache依赖与相关配置

--- stock_common/src/main/java/org/qin/com/stock/config/CommonConfig.java
+++ stock_common/src/main/java/org/qin/com/stock/config/CommonConfig.java
@@ -1,5 +1,7 @@
 package org.qin.com.stock.config;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.qin.com.stock.utils.IdWorker;
 import org.qin.com.stock.utils.ParserStockInfoUtil;
 import org.qin.com.stock.utils.TimeUtil;
@@ -54,5 +56,21 @@ public class CommonConfig {
     @Bean
     public TimeUtil timeUtil(){
         return new TimeUtil();
+    }
+    /**
+     * 构建缓存bean
+     * @return
+     */
+    @Bean
+    public Cache<String,Object> caffeineCache(){
+        Cache<String, Object> cache = Caffeine
+                .newBuilder()
+                .maximumSize(200)//设置缓存数量上限
+//                .expireAfterAccess(1, TimeUnit.SECONDS)//访问1秒后删除
+//                .expireAfterWrite(1,TimeUnit.SECONDS)//写入1秒后删除
+                .initialCapacity(100)// 初始的缓存空间大小
+                .recordStats()//开启统计
+                .build();
+        return cache;
     }
 }
